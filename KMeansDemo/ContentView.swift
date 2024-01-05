47c285477acf5c0137503987017682db471c082f
Vadim Dagman <vadim@toptal.com>
2024-01-05 10:28:30 -0800

    initial commit

---
+++ KMeansDemo/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  KMeansDemo
+//
+//  Created by Vadim on 1/5/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        HStack {
+            OriginalImageView()
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ KMeansDemo/KMeansDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  KMeansDemoApp.swift
+//  KMeansDemo
+//
+//  Created by Vadim on 1/5/24.
+//
+
+import SwiftUI
+
+@main
+struct KMeansDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
